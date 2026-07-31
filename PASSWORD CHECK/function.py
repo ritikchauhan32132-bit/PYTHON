c0497e84efc9e70cbb6f6c591779fd4b77cda8de
Ritik Kumar <ritikchauhan32132@gmail.com>
2026-07-30 21:20:36 -0700

    Auto Backup Thu 07/30/2026 21:20:35.97

---
+++ PASSWORD CHECK/function.py
@@ -0,0 +1,7 @@
+def table(n):
+    for i in range(1,11):
+        print(n*i)
+        
+
+tam = table(5)
+print(tam)
---
+++ PASSWORD CHECK/main.py
@@ -0,0 +1,15 @@
+ussername = "ritik04"
+password = "ritik0000"
+
+while True:
+    usser = input("Enter Ussename: ")
+    print()
+    passd = input("Enter Password: ")
+    print()
+
+    if(usser == ussername and passd == password):
+        print("Correct!✅")
+        break
+    else:
+        print("Incorrect Input!❎")
+        print()
