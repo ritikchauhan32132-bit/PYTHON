559d1c602d158fb0061751aa5bbf2ec5a3283f80
Ritik Kumar <ritikchauhan32132@gmail.com>
2026-08-05 21:03:16 -0700

    Auto Backup Wed 08/05/2026 21:03:16.70

---
+++ Dicsnory/main.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ").title().split()
+age = int(input("Enter Age: "))
+skill = []
+data = input("Enter your Skills: ")
+skill.append(data)
+
+dics = {
+    "name" : name,
+    "Age" : age,
+    "Skill's" : skill
+}
